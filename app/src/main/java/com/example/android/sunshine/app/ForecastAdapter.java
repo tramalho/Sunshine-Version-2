01fb5ab7a2a2d478b1c30c462fab51ffd2d25f08
trama <thiagofacul@gmail.com>
2016-07-31 22:14:11 -0300

    [Layouts Elaborados] Construção do item de lista do dia

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -45,7 +45,7 @@ public class ForecastAdapter extends CursorAdapter {
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast_today, parent, false);
     }
 
     /*
