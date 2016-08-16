ec086e8abf22e33b4c6fb2986485735eb94b2f10
trama <thiagofacul@gmail.com>
2016-08-15 21:02:33 -0300

    [Layouts Elaborados] Modificar ForecastAdapter

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -6,6 +6,7 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -45,19 +46,46 @@ public class ForecastAdapter extends CursorAdapter {
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast_today, parent, false);
+        return LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
     }
 
     /*
-        This is where we fill-in the views with the contents of the cursor.
-     */
+           This is where we fill-in the views with the contents of the cursor.
+        */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
 
+        // Read weather icon ID from cursor
+        int weatherId = cursor.getInt(ForecastFragment.COL_WEATHER_ID);
+        // Use placeholder image for now
+        ImageView iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+        iconView.setImageResource(R.drawable.ic_launcher);
 
-        TextView tv = (TextView) view.findViewById(R.id.list_item_date_textview);
-        tv.setText(convertCursorRowToUXFormat(cursor));
+        // TODO Read date from cursor
+        long longDate = cursor.getLong(ForecastFragment.COL_WEATHER_DATE);
+        TextView dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
+        dateView.setText(Utility.getFriendlyDayString(this.mContext, longDate));
+
+        // TODO Read weather forecast from cursor
+        String desc = cursor.getString(ForecastFragment.COL_WEATHER_DESC);
+        TextView forecastView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+        forecastView.setText(desc);
+
+        // Read user preference for metric or imperial temperature units
+        boolean isMetric = Utility.isMetric(context);
+
+        // Read high temperature from cursor
+        double high = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
+        TextView highView = (TextView) view.findViewById(R.id.list_item_high_textview);
+        highView.setText(Utility.formatTemperature(high, isMetric));
+
+        // TODO Read low temperature from cursor
+        double low = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
+        TextView lowView = (TextView) view.findViewById(R.id.list_item_low_textview);
+        lowView.setText(Utility.formatTemperature(low, isMetric));
+
+
     }
 }
